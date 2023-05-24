1dc7ac51137d1a8af62dd30d32cdcab2049d8864
perico3372 <117478891+perico3372@users.noreply.github.com>
2023-05-24 06:39:44 -0300

    Update Evolucion_acidentes_y_fallecidos.py

--- pages/Evolucion_aciidentes_y_fallecidos.py
+++ pages/Evolucion_aciidentes_y_fallecidos.py
@@ -20,7 +20,7 @@ data = pd.read_csv("https://raw.githubusercontent.com/perico3372/proyecto_indivi
 col1, col2 = st.columns(2)
 
 # Aplicar filtro en los datos
-limite_inferior_accidents_number = st.slider("Definir", 1970, 1980, 2021)
+limite_inferior_accidents_number = st.slider("Definir limite inferior(Año)", 1970, 1980, 2021)
 accidents_number = data[data['year'] > limite_inferior_accidents_number]
 
 diff_accidents_number = np.diff(accidents_number["num_accidents"], prepend=1)
@@ -35,7 +35,6 @@ fig_accidents_number = go.Figure(go.Waterfall(
     #text=df['Valor'],
     connector={"line": {"color": "rgb(63, 63, 63)"}},
 ))
-
 
 x_line = accidents_number['year']
 y_line = accidents_number["num_accidents"]
@@ -58,10 +57,7 @@ fig_accidents_number.update_layout(
     yaxis_title="cantidad accidentes por año",
 )
 
-
 col1.plotly_chart(fig_accidents_number, use_container_width=True, width= 1000)
-
-
 
 data_5years = pd.read_csv("https://raw.githubusercontent.com/perico3372/proyecto_individual_2/main/pages/dataframe_5years.csv")
 
@@ -85,10 +81,7 @@ fig_accidents_number_5years.add_trace(go.Scatter(
     x=x_line,
     y=y_line,
     mode='lines',
-    name='Línea',
     ))
-
-
 
 # Personalizar el gráfico
 fig_accidents_number_5years.update_layout(
@@ -105,12 +98,11 @@ fig_accidents_number_5years.update_layout(
 
 col2.plotly_chart(fig_accidents_number_5years, use_container_width=True, width= 1000)
 
-
 col1.markdown("<style>div.stPlotlyChart {width: 2000px !important;}</style>", unsafe_allow_html=True)
 col2.markdown("<style>div.stPlotlyChart {width: 2000px !important;}</style>", unsafe_allow_html=True)
 
 # Aplicar filtro en los datos
-limite_inferior_fatalities_number = st.slider("Definir 1", 1970, 1980, 2021)
+limite_inferior_fatalities_number = st.slider("Definir 1", 1970, 1970, 2021)
 fatalities_number = data[data['year'] > limite_inferior_fatalities_number]
 
 diff_fatalities_number = np.diff(fatalities_number["fatalities"], prepend=1)
